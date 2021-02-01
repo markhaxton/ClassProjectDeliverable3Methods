20e802497b804c1ffaca48f2c2d7974ff803a57d
MARKH <MARKhaxton@hotmail.com>
2021-02-01 11:37:07 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+    Author: Mark Haxton
+    Date: 1/30/2021
+    Comments: This C# Console application code demonstrates the use of
+              methods after getting input from a user.  
+*/
+using System;
+
+namespace ClassProjectDeliverable3Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a sample of a method.");
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey(true);
+            MyMethod();
+
+        }
+        static void MyMethod()
+        {
+            Console.Write("Please enter your name: ");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("Hello " + yourName + "!");
+            
+        }
+
+    } // End of class
+} // End of namespace

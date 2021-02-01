76682ca07e9440c284d77a81b92b76a3aff4936d
MARKH <MARKhaxton@hotmail.com>
2021-02-01 11:43:15 -0500

    Add code to GitHub

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿/*
     Author: Mark Haxton
-    Date: 1/30/2021
+    Date: 2/1/2021
     Comments: This C# Console application code demonstrates the use of
               methods after getting input from a user.  
 */
@@ -14,17 +14,17 @@ namespace ClassProjectDeliverable3Methods
         {
             Console.WriteLine("This is a sample of a method.");
             Console.WriteLine("Press any key to continue.");
-            Console.ReadKey(true);
-            MyMethod();
+            Console.ReadKey(true); //This pauses the app until user interacts
+            MyMethod(); //This calls the method
 
-        }
-        static void MyMethod()
+        } //end of main
+        static void MyMethod()  //This is the method which prints a persons name
         {
             Console.Write("Please enter your name: ");
-            string yourName = Console.ReadLine();
-            Console.WriteLine("Hello " + yourName + "!");
+            string yourName = Console.ReadLine(); //Variable to store users name
+            Console.WriteLine("Hello " + yourName + "!"); //Prints the name
             
-        }
+        } //end of method
 
     } // End of class
 } // End of namespace
